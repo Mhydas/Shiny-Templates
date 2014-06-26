cedd578b940344e55f7892064a64b29f4c0187f0
Eric Shure <eric@ericshure.com>
2014-06-26 12:31:41 -0700

    Initial Commit

---
+++ Dashboard/server.R
@@ -0,0 +1,7 @@
+
+shinyServer(function(input, output) {
+        output$Tab1 <- renderText({
+                rep(paste("You have chosen", input$daterange[1], "to", 
+                      input$daterange[2]), input$numbers)
+                })
+})
---
+++ Dashboard/ui.R
@@ -0,0 +1,19 @@
+
+
+shinyUI(navbarPage('My Dashboard',
+        tabPanel('SideBar Layout',
+                 sidebarLayout(
+                        sidebarPanel(h1('Buttons'),
+                                dateRangeInput('daterange',label = 'Date Range:'),
+                                numericInput('numbers', 'How Many',1, min = 1, max = 10),
+                                submitButton('Submit')
+                        ),
+                        mainPanel(h1('Output'),
+                                textOutput('Tab1')         
+                        ),
+                       
+                )
+        ),
+        tabPanel('Tab 2'),
+        tabPanel('Tab 3')
+))
